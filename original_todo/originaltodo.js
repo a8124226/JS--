93bc3021673bf0a82fd2640da62d75ee7d3f0c8c
Yudai Yama <a8124226@gsuite.si.aoyama.ac.jp>
2026-04-14 17:10:08 +0900

    定数を定義した

---
+++ original_todo/originaltodo.js
@@ -0,0 +1,7 @@
+//  <form type ="text" id="input"></form>
+//     <ul id="ul"></ul>
+
+const $form = document.getElementById('form');
+const $input = document.getAnimations('input');
+const $ul = document.getElementById('ul');
+
